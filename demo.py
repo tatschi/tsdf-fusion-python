5bd232176d156ef24ef749c5083632a46a78d083
Natascha may <natascha.may@outlook.de>
2023-10-19 13:16:26 +0200

    refactor: reformat files

--- demo.py
+++ demo.py
@@ -8,60 +8,59 @@ import numpy as np
 
 import fusion
 
-
 if __name__ == "__main__":
-  # ======================================================================================================== #
-  # (Optional) This is an example of how to compute the 3D bounds
-  # in world coordinates of the convex hull of all camera view
-  # frustums in the dataset
-  # ======================================================================================================== #
-  print("Estimating voxel volume bounds...")
-  n_imgs = 1000
-  cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
-  vol_bnds = np.zeros((3,2))
-  for i in range(n_imgs):
-    # Read depth image and camera pose
-    depth_im = cv2.imread("data/frame-%06d.depth.png"%(i),-1).astype(float)
-    depth_im /= 1000.  # depth is saved in 16-bit PNG in millimeters
-    depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
-    cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
+    # ======================================================================================================== #
+    # (Optional) This is an example of how to compute the 3D bounds
+    # in world coordinates of the convex hull of all camera view
+    # frustums in the dataset
+    # ======================================================================================================== #
+    print("Estimating voxel volume bounds...")
+    n_imgs = 1000
+    cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
+    vol_bnds = np.zeros((3, 2))
+    for i in range(n_imgs):
+        # Read depth image and camera pose
+        depth_im = cv2.imread("data/frame-%06d.depth.png" % i, -1).astype(float)
+        depth_im /= 1000.  # depth is saved in 16-bit PNG in millimeters
+        depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
+        cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % i)  # 4x4 rigid transformation matrix
 
-    # Compute camera view frustum and extend convex hull
-    view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
-    vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
-    vol_bnds[:,1] = np.maximum(vol_bnds[:,1], np.amax(view_frust_pts, axis=1))
-  # ======================================================================================================== #
+        # Compute camera view frustum and extend convex hull
+        view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
+        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
+        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
+    # ======================================================================================================== #
 
-  # ======================================================================================================== #
-  # Integrate
-  # ======================================================================================================== #
-  # Initialize voxel volume
-  print("Initializing voxel volume...")
-  tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.02)
+    # ======================================================================================================== #
+    # Integrate
+    # ======================================================================================================== #
+    # Initialize voxel volume
+    print("Initializing voxel volume...")
+    tsdf_vol = fusion.TSDFVolume(vol_bnds, voxel_size=0.02)
 
-  # Loop through images and fuse them together
-  t0_elapse = time.time()
-  for i in range(n_imgs):
-    print("Fusing frame %d/%d"%(i+1, n_imgs))
+    # Loop through images and fuse them together
+    t0_elapse = time.time()
+    for i in range(n_imgs):
+        print("Fusing frame %d/%d" % (i + 1, n_imgs))
 
-    # Read RGB-D image and camera pose
-    depth_im = cv2.imread("data/frame-%06d.depth.png"%(i),-1).astype(float)
-    depth_im /= 1000.
-    depth_im[depth_im == 65.535] = 0
-    cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))
+        # Read RGB-D image and camera pose
+        depth_im = cv2.imread("data/frame-%06d.depth.png" % i, -1).astype(float)
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = np.loadtxt("data/frame-%06d.pose.txt" % i)
 
-    # Integrate observation into voxel volume
-    tsdf_vol.integrate(depth_im, cam_intr, cam_pose, obs_weight=1.)
+        # Integrate observation into voxel volume
+        tsdf_vol.integrate(depth_im, cam_intr, cam_pose, obs_weight=1.)
 
-  fps = n_imgs / (time.time() - t0_elapse)
-  print("Average FPS: {:.2f}".format(fps))
+    fps = n_imgs / (time.time() - t0_elapse)
+    print("Average FPS: {:.2f}".format(fps))
 
-  # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
-  print("Saving mesh to mesh.ply...")
-  verts, faces, norms, colors = tsdf_vol.get_mesh()
-  fusion.meshwrite("mesh.ply", verts, faces, norms, colors)
+    # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
+    print("Saving mesh to mesh.ply...")
+    verts, faces, norms, colors = tsdf_vol.get_mesh()
+    fusion.meshwrite("mesh.ply", verts, faces, norms, colors)
 
-  # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
-  print("Saving point cloud to pc.ply...")
-  point_cloud = tsdf_vol.get_point_cloud()
-  fusion.pcwrite("pc.ply", point_cloud)
+    # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
+    print("Saving point cloud to pc.ply...")
+    point_cloud = tsdf_vol.get_point_cloud()
+    fusion.pcwrite("pc.ply", point_cloud)
