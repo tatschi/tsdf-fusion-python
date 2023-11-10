815a99e27d1a110c6fe5cb26d9a98c318ac309f2
Natascha May <85071639+tatschi@users.noreply.github.com>
2023-11-10 11:23:18 +0100

    init

--- demo.py
+++ demo.py
@@ -7,34 +7,29 @@ import cv2
 import numpy as np
 
 import fusion
+import open3d as o3d
 
-DATA_PATH = "data/2023-10-13"
+DATA_PATH = "data/2023-07-10"
 CAM_INTR_PATH = os.path.join(DATA_PATH, "camera-intrinsics.txt")
-DEPTH_PATH = lambda x: os.path.join(DATA_PATH, "depth/frame-%02d.png" % x)
+DEPTH_PATH = lambda x: os.path.join(DATA_PATH, "depth/frame-%02d.ply" % x)
 CAM_POSE_PATH = lambda x: os.path.join(DATA_PATH, "pose/frame-%02d.pose.txt" % x)
 
 if __name__ == "__main__":
     # ======================================================================================================== #
     # (Optional) This is an example of how to compute the 3D bounds
-    # in world coordinates of the convex hull of all camera view
-    # frustums in the dataset
+    # in world coordinates of the convex hull of all points in all point clouds
     # ======================================================================================================== #
     print("Estimating voxel volume bounds...")
     n_imgs = 1
-    cam_intr = np.loadtxt(CAM_INTR_PATH, delimiter=' ')
-    vol_bnds = np.zeros((3, 2))
+    pointclouds = []
     for i in range(n_imgs):
         # Read depth image and camera pose
-        depth_im = cv2.imread(DEPTH_PATH(i+1), -1).astype(float)
-        depth_im /= 1000.  # depth is saved in 16-bit PNG in millimeters
-        depth_im[depth_im == 65.535] = 0
-        cam_pose = np.loadtxt(CAM_POSE_PATH(i+1))  # 4x4 rigid transformation matrix
-        #cam_pose = np.eye(4)
+        pc = o3d.io.read_point_cloud(DEPTH_PATH(i+1))
+        pc_points = np.asarray(pc.points)
+        pointclouds.append(pc_points)
 
-        # Compute camera view frustum and extend convex hull
-        view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
-        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
-        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
+    vol_bnds = fusion.get_vol_bnds(pointclouds)
+
     # ======================================================================================================== #
 
     # ======================================================================================================== #
@@ -49,15 +44,8 @@ if __name__ == "__main__":
     for i in range(n_imgs):
         print("Fusing frame %d/%d" % (i + 1, n_imgs))
 
-        # Read RGB-D image and camera pose
-        depth_im = cv2.imread(DEPTH_PATH(i+1), -1).astype(float)
-        depth_im /= 1000.
-        depth_im[depth_im == 65.535] = 0
-        cam_pose = np.loadtxt(CAM_POSE_PATH(i+1))
-        #cam_pose = np.eye(4)
-
         # Integrate observation into voxel volume
-        tsdf_vol.integrate(depth_im, cam_intr, cam_pose, obs_weight=1.)
+        tsdf_vol.integrate(pointclouds[i], obs_weight=1.)
 
     fps = n_imgs / (time.time() - t0_elapse)
     print("Average FPS: {:.2f}".format(fps))
--- fusion.py
+++ fusion.py
@@ -189,72 +189,21 @@ class TSDFVolume:
             tsdf_vol_int[i] = (w_old[i] * tsdf_vol[i] + obs_weight * dist[i]) / w_new[i]
         return tsdf_vol_int, w_new
 
-    def integrate(self, depth_im, cam_intr, cam_pose, obs_weight=1.):
+    def integrate(self, pointcloud, obs_weight=1.):
         """Integrate a depth frame into the TSDF volume.
 
     Args:
-      depth_im (ndarray): A depth image of shape (H, W).
-      cam_intr (ndarray): The camera intrinsics matrix of shape (3, 3).
-      cam_pose (ndarray): The camera pose (i.e. extrinsics) of shape (4, 4).
+      pointcloud (ndarray): A list of 3D points.
       obs_weight (float): The weight to assign for the current observation. A higher
         value
     """
-        im_h, im_w = depth_im.shape
-
-        if self.gpu_mode:  # GPU mode: integrate voxel volume (calls CUDA kernel)
-            for gpu_loop_idx in range(self._n_gpu_loops):
-                self._cuda_integrate(self._tsdf_vol_gpu,
-                                     self._weight_vol_gpu,
-                                     cuda.InOut(self._vol_dim.astype(np.float32)),
-                                     cuda.InOut(self._vol_origin.astype(np.float32)),
-                                     cuda.InOut(cam_intr.reshape(-1).astype(np.float32)),
-                                     cuda.InOut(cam_pose.reshape(-1).astype(np.float32)),
-                                     cuda.InOut(np.asarray([
-                                         gpu_loop_idx,
-                                         self._voxel_size,
-                                         im_h,
-                                         im_w,
-                                         self._trunc_margin,
-                                         obs_weight
-                                     ], np.float32)),
-                                     cuda.InOut(depth_im.reshape(-1).astype(np.float32)),
-                                     block=(self._max_gpu_threads_per_block, 1, 1),
-                                     grid=(
-                                         int(self._max_gpu_grid_dim[0]),
-                                         int(self._max_gpu_grid_dim[1]),
-                                         int(self._max_gpu_grid_dim[2]),
-                                     )
-                                     )
-        else:  # CPU mode: integrate voxel volume (vectorized implementation)
-            # Convert voxel grid coordinates to pixel coordinates
-            cam_pts = self.vox2world(self._vol_origin, self.vox_coords, self._voxel_size)
-            cam_pts = rigid_transform(cam_pts, np.linalg.inv(cam_pose))
-            pix_z = cam_pts[:, 2]
-            pix = self.cam2pix(cam_pts, cam_intr)
-            pix_x, pix_y = pix[:, 0], pix[:, 1]
-
-            # Eliminate pixels outside view frustum
-            valid_pix = np.logical_and(pix_x >= 0,
-                                       np.logical_and(pix_x < im_w,
-                                                      np.logical_and(pix_y >= 0,
-                                                                     np.logical_and(pix_y < im_h,
-                                                                                    pix_z > 0))))
-            depth_val = np.zeros(pix_x.shape)
-            depth_val[valid_pix] = depth_im[pix_y[valid_pix], pix_x[valid_pix]]
-
-            # Integrate TSDF
-            depth_diff = depth_val - pix_z
-            valid_pts = np.logical_and(depth_val > 0, depth_diff >= -self._trunc_margin)
-            dist = np.minimum(1, depth_diff / self._trunc_margin)
-            valid_vox_x = self.vox_coords[valid_pts, 0]
-            valid_vox_y = self.vox_coords[valid_pts, 1]
-            valid_vox_z = self.vox_coords[valid_pts, 2]
-            w_old = self._weight_vol_cpu[valid_vox_x, valid_vox_y, valid_vox_z]
-            tsdf_vals = self._tsdf_vol_cpu[valid_vox_x, valid_vox_y, valid_vox_z]
-            valid_dist = dist[valid_pts]
-            tsdf_vol_new, w_new = self.integrate_tsdf(tsdf_vals, valid_dist, w_old, obs_weight)
-            self._weight_vol_cpu[valid_vox_x, valid_vox_y, valid_vox_z] = w_new
-            self._tsdf_vol_cpu[valid_vox_x, valid_vox_y, valid_vox_z] = tsdf_vol_new
+        # TODO implement GPU mode
+        world_coords = self.vox2world(self._vol_origin, self.vox_coords, self._voxel_size)
+        depth_diff = pointcloud - world_coords
+        valid_pts = depth_diff >= -self._trunc_margin
+        valid_dist = depth_diff[valid_pts]
+        self._tsdf_vol_cpu, self._weight_vol_cpu = self.integrate_tsdf(self._tsdf_vol_cpu, valid_dist,
+                                                                       self._weight_vol_cpu, obs_weight)
 
     def get_volume(self):
         if self.gpu_mode:
@@ -311,6 +260,13 @@ def get_view_frustum(depth_im, cam_intr, cam_pose):
     ])
     view_frust_pts = rigid_transform(view_frust_pts.T, cam_pose).T
     return view_frust_pts
+
+
+def get_vol_bnds(pointclouds):
+    vol_bnds = np.zeros((3, 2))
+    vol_bnds[:, 0] = np.min(pointclouds, axis=1)
+    vol_bnds[:, 1] = np.max(pointclouds, axis=1)
+    return vol_bnds
 
 
 def get_point_3d_from_depth_pixel(depth_im, cam_intr, cam_pose, x, y):
